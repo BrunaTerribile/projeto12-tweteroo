f785445ca2893f8827745d3d2c30eb89846a050c
Bruna Terribile <brunaterribile@gmail.com>
2022-11-07 15:27:39 -0300

    Get /tweets - renderizar apenas 10 tweets

--- index.js
+++ index.js
@@ -5,35 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-let users = [
-    {
-        username: "bobesponja",
-        avatar: "https://yt3.ggpht.com/ytc/AMLnZu9tYPIG3bxki2LZz-NRrvHtLHRL0-wW95Cjgcr2=s900-c-k-c0x00ffffff-no-rj"
-    },
-    {
-        username: "patrick",
-        avatar: "https://upload.wikimedia.org/wikipedia/pt/b/b1/Patrick_Estrela.png"
-    },
-    {
-        username: "sirigueijo",
-        avatar: "https://www.pintarcolorir.com.br/wp-content/uploads/2015/11/desenhos-de-seu-sirigueijo-para-colorir-imagem-172x172.gif"
-    }
-]
-
-const tweets = [
-    {
-        username: "bobesponja",
-        tweet: "eu amo o hub",
-    },
-    {
-        username: "patrick",
-        tweet: "bob esponja, vamos caçar agua-viva?",
-    },
-    {
-        username: "sirigueijo",
-        tweet: "dinheiro, dinheiro, dinheiro!",
-    }
-]
+let users = []
+let tweets = []
 
 app.post("/sign-up", (req, res) => {
     const userData = {
@@ -51,19 +24,18 @@ app.post("/tweets", (req, res) => {
         tweet: req.body.tweet
     };
 
-    tweets.push(newTweet)
+    tweets.unshift(newTweet)
     res.send("OK")
 })
 
 app.get("/tweets", (req, res) => {
-
     const render = tweets.map((t) => ({
         username: (t.username),
         tweet: (t.tweet),
         avatar: (users.find((u) => u.username === t.username)).avatar,
     }))
 
-    res.send(render)
+    res.send(render.slice(0, 10))
 })
 
 app.listen(5000, () => {console.log("Server running in port: 5000")})
