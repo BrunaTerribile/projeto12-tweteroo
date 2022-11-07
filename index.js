9bfa6be5a6441e587e889bab08bfd7442862b32b
Bruna Terribile <brunaterribile@gmail.com>
2022-11-07 14:33:02 -0300

    Post /sign-up

--- index.js
+++ index.js
@@ -3,10 +3,47 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
-app.get("/hello", (req, res) => {
-    const oi = "Hello World!"
-    res.send(oi)
+let users = [
+    {
+        username: "bobesponja",
+        avatar: "https://static.wikia.nocookie.net/nickelodeon6666/images/1/18/BobEsponja.png/revision/latest?cb=20160421162154&path-prefix=pt-br"
+    },
+    {
+        username: "patrick",
+        avatar: "https://upload.wikimedia.org/wikipedia/pt/b/b1/Patrick_Estrela.png"
+    },
+    {
+        username: "sirigueijo",
+        avatar: "https://upload.wikimedia.org/wikipedia/pt/b/b1/Patrick_Estrela.png"
+    }
+]
+
+const tweets = [
+    {
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        username: "patrick",
+        tweet: "bob esponja, vamos caçar agua-viva?"
+    },
+    {
+        username: "sirigueijo",
+        tweet: "dinheiro, dinheiro, dinheiro!"
+    }
+]
+
+app.post("/sign-up", (req, res) => {
+    const userData = {
+        username: req.body.username,
+        avatar: req.body.avatar
+    }
+
+    console.log(userData)
+    users.push(userData)
+    res.send("OK")
 })
 
 app.listen(5000, () => {console.log("Server running in port: 5000")})
